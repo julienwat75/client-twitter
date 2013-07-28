668d4435162b6d1ea40be560fd8b1571aee6f3cd
Andrei Vladescu-Olt <andrei.volt@gmail.com>
2013-07-28 04:31:11 +0200

    une redirection !

--- twitter.rb
+++ twitter.rb
@@ -21,7 +21,5 @@ end
 get "/bienvenue" do
   pseudo = params["pseudo"]
   response.set_cookie('pseudo', pseudo)
-  "Bienvenue sur Twitter, #{pseudo} !
-
-   Revenir à l'<a href='/'>accueil</a>"
+  redirect '/'
 end
